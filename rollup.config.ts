af07bb41daaaaf76a9da82bdacc94b1e832eabc1
RashingPro <rashingpro@yandex.ru>
2025-10-29 19:09:19 +0300

    build: implement declarations generation and cleaning `dist` directory

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,16 +2,21 @@ import typescriptPlugin, { RollupTypescriptOptions } from "@rollup/plugin-typesc
 import terserPlugin from "@rollup/plugin-terser";
 import { RollupOptions } from "rollup";
 
+const outDir = "dist";
+
 export default {
     input: "src/index.ts",
     output: {
-        dir: "dist",
+        dir: outDir,
         format: "es",
         sourcemap: true
     },
     plugins: [
         typescriptPlugin({
-            noEmitOnError: true
+            noEmitOnError: true,
+            include: ["src/**/*.ts"],
+            declaration: true,
+            outDir: outDir
         }),
         terserPlugin({
             keep_classnames: /^.*Error$/
