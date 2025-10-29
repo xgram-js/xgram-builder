184c3012afc1cddbb2e718e4c8a14ad01e697c7c
RashingPro <rashingpro@yandex.ru>
2025-10-30 00:25:42 +0300

    build: implement `.d.ts` minification

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,9 +1,27 @@
 import typescriptPlugin, { RollupTypescriptOptions } from "@rollup/plugin-typescript";
 import terserPlugin from "@rollup/plugin-terser";
-import { RollupOptions } from "rollup";
+import { RollupOptions, Plugin } from "rollup";
 import { dts as dtsPlugin } from "rollup-plugin-dts";
+import { createMinifier } from "dts-minify";
+import ts from "typescript";
 
 const outDir = "dist";
+
+function dtsMinifyPlugin(): Plugin {
+    return {
+        name: "dts-minify",
+        generateBundle: (options, bundle) => {
+            const minifier = createMinifier(ts);
+
+            for (const [fileName, file] of Object.entries(bundle)) {
+                if (fileName.endsWith(".d.ts") && file.type === "chunk") {
+                    file.code = minifier.minify(file.code);
+                    console.log(`Minified ${fileName}`);
+                }
+            }
+        }
+    };
+}
 
 export default [
     {
@@ -28,6 +46,6 @@ export default [
             dir: outDir,
             format: "es"
         },
-        plugins: [dtsPlugin()]
+        plugins: [dtsPlugin(), dtsMinifyPlugin()]
     }
 ] as (RollupOptions & RollupTypescriptOptions)[];
