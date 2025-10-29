846752b5c30156991b0c3b12b4a85ac190169086
RashingPro <rashingpro@yandex.ru>
2025-10-29 19:23:54 +0300

    feat: implement optional entries in export maps

--- src/exportMaps.ts
+++ src/exportMaps.ts
@@ -3,21 +3,33 @@ import assert from "node:assert";
 import { InvalidProjectStructureError } from "./mapper";
 import chalk from "chalk";
 
-export type ExportMaps = Record<string, PrimitiveTypeLiteral>;
+export type ExportMapDeclaration = {
+    type: PrimitiveTypeLiteral;
+    required: boolean;
+};
+export type ExportMaps = Record<string, ExportMapDeclaration>;
 
 export default {
     command: {
-        default: "function"
+        default: {
+            type: "function",
+            required: true
+        },
+        commandConfig: {
+            type: "object",
+            required: false
+        }
     }
 } as Record<string, ExportMaps>;
 
 export function assertExportsToMap(map: ExportMaps, exports: any, file: string) {
     Object.keys(map).forEach(key => {
+        if (!map[key].required && typeof exports[key] === "undefined") return;
         assert(
-            typeof exports[key] == map[key],
+            typeof exports[key] == map[key].type,
             new InvalidProjectStructureError(
                 file,
-                `to contain export key ${chalk.green(key)} with type ${chalk.green(map[key])}`,
+                `to contain ${map[key].required ? "" : chalk.yellow("optional ")}export key ${chalk.green(key)} with type ${chalk.green(map[key].type)}`,
                 typeof exports[key] == "undefined" ? null : typeof exports[key]
             )
         );
