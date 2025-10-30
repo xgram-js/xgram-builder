e05cc03afbde58716b3f4dd6c27d75f4e4887acf
RashingPro <rashingpro@yandex.ru>
2025-10-30 13:09:26 +0300

    feat: add command registration to `virtual-index.ts`

--- skeleton/virtual-index.ts
+++ skeleton/virtual-index.ts
@@ -8,6 +8,8 @@ async function main() {
     configDotenv({ quiet: true });
     assert(process.env.TOKEN, "Bot token was not found in .env");
     const bot = new Bot(process.env.TOKEN);
+    commands.forEach(bot.registerCommand.bind(bot));
+    bot.startPolling();
 }
 
 void main();
